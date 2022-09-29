a0213ca36eb5240468f78f714839758e0c1d0f42
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-28 19:29:47 -0500

    Simple for sentence

---
+++ src/SentenciaFor.java
@@ -0,0 +1,10 @@
+public class SentenciaFor {
+    public static void main(String[] args) {
+
+        int i = 0;
+        for (; i < 5; i++)
+            System.out.println("Hola " + i);
+
+        System.out.println(i);
+    }
+}
